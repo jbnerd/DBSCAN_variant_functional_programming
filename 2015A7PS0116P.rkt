cc5eb070b20dad750887ffbf1c09938e6e6fdd36
jbnerd <abhivjoshi.aj@gmail.com>
2017-11-07 17:09:00 +0530

    Step2 completed.

--- 2015A7PS0116P.rkt
+++ 2015A7PS0116P.rkt
@@ -29,10 +29,53 @@
     (cons (append (list idx) (list (car ls))) (add_index (cdr ls) (+ idx 1)))
   )
 )
-
 ;(define temp_list (get_inp '(0 1 2 3 4 5 6 7 8 9 10 11) 4))
 ;temp_list
 ;(add_index temp_list 0)
+
+(define (sq_dist ls1 ls2)
+  (if (and (null? ls1) (null? ls2)) 0
+    (+ (expt (- (car ls1) (car ls2)) 2) (sq_dist (cdr ls1) (cdr ls2)))
+  )
+)
+
+(define (similarity ls1 ls2)
+  (sqrt (sq_dist ls1 ls2))
+)
+;(similarity '(1 1 1) '(2 2 2))
+
+(define (point_eq point1 point2)
+  (cond
+    ((and (null? point1) (null? point2)) #t)
+    ((not (= (car point1) (car point2))) #f)
+    (else (point_eq (cdr point1) (cdr point2))) 
+  )
+)
+;(point_eq '(0 0) '(0 0))
+;(point_eq (cdr (list '(1 (0 0)))) (cdr (list '(1 (0 0)))))
+
+(define (find_distances point point_list idx)
+  (cond
+    ((null? point_list) '())
+    ((point_eq point (car point_list)) (cons (append (list idx) (list +inf.0)) (find_distances point (cdr point_list) (+ idx 1)))) 
+    (else (cons (append (list idx) (list (similarity point (car point_list)))) (find_distances point (cdr point_list) (+ idx 1))))
+  )
+)
+;(find_distances '(0 0) '((0 0) (0 1) (1 0) (1 1)) 1)
+
+(define (sim_matrix point_list idx len)
+  (if (= idx len) '()
+      (cons (find_distances (list-ref point_list idx) point_list 1) (sim_matrix point_list (+ idx 1) len))
+  )
+)
+;(sim_matrix '((0 1) (1 0) (1 1)) 0 3)
+
+(define (del_indices point_list)
+  (if (null? point_list) '()
+    (append (cdr (car point_list)) (del_indices (cdr point_list)))
+  )
+)
+
 
 (define file_list (file->list "./t0.in"))
 ;file_list
@@ -44,4 +87,8 @@
 (define raw_inp_list (truncate_list file_list 5))
 (define inp_list (get_inp raw_inp_list D))
 (define step1 (add_index inp_list 1))
-step1
+;step1
+;(define temp (del_indices step1))
+;temp
+(define step2 (sim_matrix (del_indices step1) 0 N))
+;step2
